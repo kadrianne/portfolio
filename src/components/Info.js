fa824121815a76a81afe6f17d7128a79a0c8c8f7
Kristine Du <kristine.a.du@gmail.com>
2020-10-08 17:55:48 -0600

    Simplify components.

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import Header from './Header'
 
-const Info = () => (
-    <div class='main'>
-        <Header />
-        <footer>
-            © {new Date().getFullYear()}
-        </footer>
+const Info = ({ text, setPage }) => (
+    <div className='info'>
+      <header>
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+            <a onClick={() => setPage('about')}>about</a>
+            <a onClick={() => setPage('projects')}>projects</a>
+        </nav>
+        <h1 className='blue'>kristine <span>{text}</span></h1>
+      </header>
+      <footer>
+          © {new Date().getFullYear()}
+      </footer>
     </div>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/mystyles.scss'
 import SEO from '../components/seo'
 import Info from '../components/Info'
@@ -6,10 +6,18 @@ import Home from '../components/Home'
 import Projects from '../components/Projects'
 
 const App = () => {
+
+  const [page, setPage] = useState('about')
+
+  const texts = {
+    about: 'codes',
+    projects: 'builds'
+  }
+
   return (
     <>
       <SEO title="Kristine Codes - Homepage" />
-      <Info />
+      <Info text={texts[page]} setPage={setPage} />
       <main>
           <Home />
           <Projects />
