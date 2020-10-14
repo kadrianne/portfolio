90ea7b172e102bf26f72edc2379d79818b9b004a
Kristine Du <kristine.a.du@gmail.com>
2020-10-14 13:52:41 -0600

    Add logic to stop calculations when component is out of window view.

--- src/components/About.js
+++ src/components/About.js
@@ -1,26 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import calculateColorValue from '../helpers/calculateColorValue'
 
 const About = ({ setRed, setGreen, setBlue, startingColor, finalColor }) => {
+
+  const [height, setHeight] = useState(0)
 
   const [ startingRed, startingGreen, startingBlue ] = startingColor
   const [ finalRed, finalGreen, finalBlue ] = finalColor
 
-  const calculateColorValue = (startingColor, finalColor) => {
-    const height = document.querySelector('.about').clientHeight
-    const scrollPosition = ((height + window.scrollY) / height) - 1
-    return Math.round(startingColor + ((finalColor - startingColor) * scrollPosition))
-  }
-  
   const handleScroll = () => {
-    setRed(calculateColorValue(startingRed, finalRed))
-    setGreen(calculateColorValue(startingGreen, finalGreen))
-    setBlue(calculateColorValue(startingBlue, finalBlue))
+    if (window.scrollY <= height){
+      setRed(calculateColorValue(height, startingRed, finalRed))
+      setGreen(calculateColorValue(height, startingGreen, finalGreen))
+      setBlue(calculateColorValue(height, startingBlue, finalBlue))
+    }
   }
+
+  useEffect(() => {
+    const height = document.querySelector('.about').clientHeight
+    setHeight(height)
+  }, [])
 
   return (
     <>
+    {window.addEventListener('scroll', handleScroll)} 
     <section className='about'>
-    {window.addEventListener('scroll', handleScroll)}
         <h2>About</h2>
         <img src='https://i.kym-cdn.com/photos/images/original/001/295/524/cda.jpg' ></img>
     </section>
