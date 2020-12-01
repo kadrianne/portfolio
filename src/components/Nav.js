08b814dc8e4b017c4bd10a0dfad80373c63bdc12
Kristine Du <kristine.a.du@gmail.com>
2020-11-30 21:33:48 -0700

    Update responsiveness on info section and fix colors on blogs and mobile address bar.

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import resume from '../assets/kristine-du-resume.pdf'
+
+const Info = ({ aboutRef, projectsRef, blogsRef, contactRef, handleClick }) => {
+  return (
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <li><a onClick={() => handleClick(aboutRef.current)}>about</a></li>
+      <li><a onClick={() => handleClick(projectsRef.current)}>projects</a></li>
+      <li><a onClick={() => handleClick(blogsRef.current)}>blogs</a></li>
+      <li><a onClick={() => handleClick(contactRef.current)}>contact</a></li>
+      <li><a href={resume} target='_blank'>resume</a></li>
+    </nav>
+  )
+}
+
+export default Info
--- src/pages/index.js
+++ src/pages/index.js
@@ -22,6 +22,7 @@ const componentColors = {
 }
 
 const App = () => {
+  const [width, setWidth] = useState(0)
   const [heights, addHeight] = useState({})
   const [componentLoaded, setComponentLoaded] = useState({
     about: false,
@@ -82,6 +83,10 @@ const App = () => {
   }
 
   useEffect(() => {
+    setWidth(window.screen.width)
+  }, [])
+
+  useEffect(() => {
     if (Object.keys(heights).length === 4) {
       window.addEventListener('scroll', handleColors)
     }
@@ -91,6 +96,7 @@ const App = () => {
     <>
       <SEO title="kristine codes" />
       <Info
+        width={width}
         red={red}
         green={green}
         blue={blue}
@@ -101,12 +107,14 @@ const App = () => {
         handleClick={handleClick}
       />
       <main style={{backgroundColor: `rgb(${red},${green},${blue})`}}>
+        {console.log(window.scrollY)}
         <FontAwesomeIcon className='scroll-to-top' icon={faArrowCircleUp} size='2x' onClick={() => handleClick()} />
         <About aboutRef={aboutRef} heights={heights} addHeight={addHeight} componentLoaded={componentLoaded} setComponentLoaded={setComponentLoaded} />
         <Projects projectsRef={projectsRef} heights={heights} addHeight={addHeight} componentLoaded={componentLoaded} setComponentLoaded={setComponentLoaded} />
         <Blogs blogsRef={blogsRef} heights={heights} addHeight={addHeight} componentLoaded={componentLoaded} setComponentLoaded={setComponentLoaded} />
         <Contact contactRef={contactRef} heights={heights} addHeight={addHeight} componentLoaded={componentLoaded} setComponentLoaded={setComponentLoaded} />
       </main>
+      { width <=1024 && <footer className='device' style={{backgroundColor: `rgb(${red},${green},${blue})`}}>© {new Date().getFullYear()}</footer>}
     </>
   )
 }
